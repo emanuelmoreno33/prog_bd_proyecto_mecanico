05cf9b7aad7e8f450c434c3297e3477fc943f7ac
CinthiaBV <cinthia.vargas18@tectijuana.edu.mx>
2020-10-09 22:16:22 -0700

    Agrege el pedazo de interfaz

---
+++ Taller_Mecanico/Taller_Mecanico/Frm_Login.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace Taller_Mecanico
+{
+    partial class Frm_Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frm_Login));
+            this.bunifuElipse1 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.pnl_titulo = new System.Windows.Forms.Panel();
+            this.pnl_imagen = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.elDivider1 = new Klik.Windows.Forms.v1.EntryLib.ELDivider();
+            this.txtusu = new Klik.Windows.Forms.v1.EntryLib.ELEntryBox();
+            this.txtpass = new Klik.Windows.Forms.v1.EntryLib.ELEntryBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.elButton1 = new Klik.Windows.Forms.v1.EntryLib.ELButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.elDivider1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtusu)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtpass)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.elButton1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bunifuElipse1
+            // 
+            this.bunifuElipse1.ElipseRadius = 20;
+            this.bunifuElipse1.TargetControl = this;
+            // 
+            // pnl_titulo
+            // 
+            this.pnl_titulo.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.pnl_titulo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnl_titulo.Location = new System.Drawing.Point(0, 0);
+            this.pnl_titulo.Name = "pnl_titulo";
+            this.pnl_titulo.Size = new System.Drawing.Size(800, 30);
+            this.pnl_titulo.TabIndex = 0;
+            // 
+            // pnl_imagen
+            // 
+            this.pnl_imagen.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pnl_imagen.BackgroundImage")));
+            this.pnl_imagen.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pnl_imagen.Dock = System.Windows.Forms.DockStyle.Right;
+            this.pnl_imagen.Location = new System.Drawing.Point(397, 30);
+            this.pnl_imagen.Name = "pnl_imagen";
+            this.pnl_imagen.Size = new System.Drawing.Size(403, 420);
+            this.pnl_imagen.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(139, 36);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(113, 113);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(65, 166);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(267, 32);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "TALLER MECÁNICO ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(131, 198);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 32);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "DORYO ";
+            // 
+            // elDivider1
+            // 
+            this.elDivider1.FadeStyle = Klik.Windows.Forms.v1.EntryLib.DividerFadeStyles.Center;
+            this.elDivider1.LineColor = System.Drawing.Color.Gainsboro;
+            this.elDivider1.Location = new System.Drawing.Point(68, 238);
+            this.elDivider1.Name = "elDivider1";
+            this.elDivider1.Size = new System.Drawing.Size(263, 9);
+            this.elDivider1.TabIndex = 5;
+            this.elDivider1.VisualStyle = Klik.Windows.Forms.v1.Common.ControlVisualStyles.Custom;
+            // 
+            // txtusu
+            // 
+            this.txtusu.CaptionStyle.CaptionSize = 0;
+            this.txtusu.CaptionStyle.FlashStyle.PaintType = Klik.Windows.Forms.v1.Common.PaintTypes.Solid;
+            this.txtusu.CaptionStyle.FlashStyle.SolidColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(240)))), ((int)(((byte)(191)))));
+            this.txtusu.CaptionStyle.Office2007Scheme = Klik.Windows.Forms.v1.Common.Office2007Schemes.ModernBlack;
+            this.txtusu.CaptionStyle.TextStyle.ForeColor = System.Drawing.Color.White;
+            this.txtusu.CaptionStyle.TextStyle.Text = "elEntryBox1";
+            this.txtusu.EditBoxStyle.BorderStyle.BorderType = Klik.Windows.Forms.v1.Common.BorderTypes.None;
+            this.txtusu.EditBoxStyle.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtusu.EditBoxStyle.ForeColor = System.Drawing.Color.Gray;
+            this.txtusu.EditBoxStyle.Office2007Scheme = Klik.Windows.Forms.v1.Common.Office2007Schemes.ModernBlack;
+            this.txtusu.EditBoxStyle.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txtusu.Location = new System.Drawing.Point(68, 270);
+            this.txtusu.Name = "txtusu";
+            this.txtusu.Size = new System.Drawing.Size(247, 38);
+            this.txtusu.TabIndex = 6;
+            this.txtusu.ValidationStyle.PasswordChar = '\0';
+            this.txtusu.Value = "";
+            this.txtusu.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtusu_KeyDown);
+            // 
+            // txtpass
+            // 
+            this.txtpass.CaptionStyle.CaptionSize = 0;
+            this.txtpass.CaptionStyle.FlashStyle.PaintType = Klik.Windows.Forms.v1.Common.PaintTypes.Solid;
+            this.txtpass.CaptionStyle.FlashStyle.SolidColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(240)))), ((int)(((byte)(191)))));
+            this.txtpass.CaptionStyle.Office2007Scheme = Klik.Windows.Forms.v1.Common.Office2007Schemes.ModernBlack;
+            this.txtpass.CaptionStyle.TextStyle.ForeColor = System.Drawing.Color.White;
+            this.txtpass.CaptionStyle.TextStyle.Text = "elEntryBox1";
+            this.txtpass.EditBoxStyle.BorderStyle.BorderType = Klik.Windows.Forms.v1.Common.BorderTypes.None;
+            this.txtpass.EditBoxStyle.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtpass.EditBoxStyle.ForeColor = System.Drawing.Color.Gray;
+            this.txtpass.EditBoxStyle.Office2007Scheme = Klik.Windows.Forms.v1.Common.Office2007Schemes.ModernBlack;
+            this.txtpass.EditBoxStyle.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txtpass.Location = new System.Drawing.Point(66, 324);
+            this.txtpass.Name = "txtpass";
+            this.txtpass.Size = new System.Drawing.Size(249, 38);
+            this.txtpass.TabIndex = 7;
+            this.txtpass.ValidationStyle.PasswordChar = '\0';
+            this.txtpass.ValidationStyle.UseSystemPasswordChar = true;
+            this.txtpass.Value = "";
+            // 
+            // label3
+            // 
+            this.label3.Image = ((System.Drawing.Image)(resources.GetObject("label3.Image")));
+            this.label3.Location = new System.Drawing.Point(75, 277);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 23);
+            this.label3.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.Image = ((System.Drawing.Image)(resources.GetObject("label4.Image")));
+            this.label4.Location = new System.Drawing.Point(73, 332);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(21, 23);
+            this.label4.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(135, 418);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(110, 14);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Hecho en el ITT";
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.FlatAppearance.BorderSize = 0;
+            this.btn_exit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_exit.Image = global::Taller_Mecanico.Properties.Resources.remove_button1;
+            this.btn_exit.Location = new System.Drawing.Point(368, 31);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(26, 26);
+            this.btn_exit.TabIndex = 11;
+            this.btn_exit.UseVisualStyleBackColor = true;
+            this.btn_exit.Click += new System.EventHandler(this.btn_exit_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(273, 125);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(73, 10);
+            this.button1.TabIndex = 12;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            // 
+            // elButton1
+            // 
+            this.elButton1.BackgroundStyle.PaintType = Klik.Windows.Forms.v1.Common.PaintTypes.Solid;
+            this.elButton1.BackgroundStyle.SolidColor = System.Drawing.Color.DarkOrange;
+            this.elButton1.BorderStyle.SolidColor = System.Drawing.Color.DarkOrange;
+            this.elButton1.FlashStyle.PaintType = Klik.Windows.Forms.v1.Common.PaintTypes.Solid;
+            this.elButton1.FlashStyle.SolidColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(240)))), ((int)(((byte)(191)))));
+            this.elButton1.ForegroundImageStyle.ImageAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.elButton1.Location = new System.Drawing.Point(125, 368);
+            this.elButton1.Name = "elButton1";
+            this.elButton1.Size = new System.Drawing.Size(133, 35);
+            this.elButton1.StateStyles.HoverStyle.BackgroundGradientEndColor = System.Drawing.Color.YellowGreen;
+            this.elButton1.StateStyles.HoverStyle.BackgroundGradientStartColor = System.Drawing.Color.YellowGreen;
+            this.elButton1.StateStyles.HoverStyle.BackgroundSolidColor = System.Drawing.Color.YellowGreen;
+            this.elButton1.StateStyles.HoverStyle.BorderGradientEndColor = System.Drawing.Color.YellowGreen;
+            this.elButton1.StateStyles.HoverStyle.BorderGradientStartColor = System.Drawing.Color.YellowGreen;
+            this.elButton1.StateStyles.HoverStyle.BorderSolidColor = System.Drawing.Color.YellowGreen;
+            this.elButton1.TabIndex = 13;
+            this.elButton1.TextStyle.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.elButton1.TextStyle.ForeColor = System.Drawing.Color.White;
+            this.elButton1.TextStyle.Text = "Login";
+            this.elButton1.TextStyle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.elButton1.VisualStyle = Klik.Windows.Forms.v1.EntryLib.ButtonVisualStyles.Custom;
+            // 
+            // Frm_Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.elButton1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn_exit);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtpass);
+            this.Controls.Add(this.txtusu);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pnl_imagen);
+            this.Controls.Add(this.pnl_titulo);
+            this.Controls.Add(this.elDivider1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.KeyPreview = true;
+            this.Name = "Frm_Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.elDivider1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtusu)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtpass)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.elButton1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse1;
+        private Klik.Windows.Forms.v1.EntryLib.ELDivider elDivider1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel pnl_imagen;
+        private System.Windows.Forms.Panel pnl_titulo;
+        private System.Windows.Forms.Button btn_exit;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private Klik.Windows.Forms.v1.EntryLib.ELEntryBox txtpass;
+        private Klik.Windows.Forms.v1.EntryLib.ELEntryBox txtusu;
+        private Klik.Windows.Forms.v1.EntryLib.ELButton elButton1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Taller_Mecanico/Taller_Mecanico/Frm_Login.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Taller_Mecanico
+{
+    public partial class Frm_Login : Form
+    {
+        public Frm_Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txtusu_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode==Keys.Enter)
+            {
+                txtpass.Focus();
+            }
+        }
+    }
+}
